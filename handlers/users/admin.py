7c0622735de29a4b298ea79bcbc42ee15206662e
In0ke <lexiandr@gmail.com>
2021-04-11 12:44:03 +0300

    add some filter

---
+++ handlers/users/admin.py
@@ -0,0 +1,9 @@
+from aiogram import types
+
+from filters import IsPrivate
+from loader import dp
+
+
+@dp.message_handler(IsPrivate(), user_id=[222974391], text='secret')
+async def admin_chat_secret(message: types.Message):
+    await message.answer('Это секретное сообщение, вызванное одним из администраторов в личной переписке')
