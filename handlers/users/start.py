66fd951edd10c489518a3a5269af16ceea96423c
In0ke <lexiandr@gmail.com>
2021-04-11 16:35:05 +0300

    add some middlewares

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,19 +8,23 @@ from filters import IsPrivate
 from loader import dp
 
 
-@dp.message_handler(CommandStart(deep_link=re.compile(r"\d\d\d")), IsPrivate())
+@dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    deep_link_args = message.get_args()
-    await message.answer(f"Привет, {message.from_user.full_name}!\n"
-                         f"Вы находитесь в личной переписке \n"
-                         f"В вышей команде есть диплинк \n"
-                         f"Вы передали аргумент {deep_link_args}")
+    await message.answer(f'Привет, {message.from_user.full_name}!')
 
-
-@dp.message_handler(CommandStart(), IsPrivate())
-async def bot_start(message: types.Message):
-    deep_link = await get_start_link(payload="123")
-    await message.answer(f"Привет, {message.from_user.full_name}!\n"
-                         f"Вы находитесь в личной переписке \n"
-                         f"В вышей команде нет диплинк \n"
-                         f"Ваша диплинк ссылка - {deep_link}")
+# @dp.message_handler(CommandStart(deep_link=re.compile(r"\d\d\d")), IsPrivate())
+# async def bot_start(message: types.Message):
+#     deep_link_args = message.get_args()
+#     await message.answer(f"Привет, {message.from_user.full_name}!\n"
+#                          f"Вы находитесь в личной переписке \n"
+#                          f"В вышей команде есть диплинк \n"
+#                          f"Вы передали аргумент {deep_link_args}")
+#
+#
+# @dp.message_handler(CommandStart(), IsPrivate())
+# async def bot_start(message: types.Message):
+#     deep_link = await get_start_link(payload="123")
+#     await message.answer(f"Привет, {message.from_user.full_name}!\n"
+#                          f"Вы находитесь в личной переписке \n"
+#                          f"В вышей команде нет диплинк \n"
+#                          f"Ваша диплинк ссылка - {deep_link}")
