147ae3159fa0ecbe68edd4c390c4ada6f846d130
In0ke <lexiandr@gmail.com>
2021-04-12 20:46:13 +0300

    добавил операции над сервером

--- handlers/users/reglet_list.py
+++ handlers/users/reglet_list.py
@@ -1,7 +1,9 @@
 import requests
 from aiogram import types
 from aiogram.dispatcher.filters import Command
+from aiogram.types import CallbackQuery
 
+from keyboards.inline.options import button
 from loader import dp, db
 
 
@@ -13,5 +15,29 @@ async def get_server_list(message: types.Message):
     request = requests.get(url, headers=token)
     response = request.json()
     for value in response["reglets"]:
-        text = str([(value["name"], value["id"], value["ip"], value["image"]["name"])])
-        await message.answer(text=str(text))
+        text = str((value["name"], value["id"], value["ip"], value["image"]["name"], value['status']))
+        await message.answer(text=str(text), reply_markup=button)
+
+
+@dp.callback_query_handler(text_contains="start")
+async def buying_pear(call: CallbackQuery, reglet_id: str):
+    await call.answer(cache_time=60)
+    pass
+
+
+@dp.callback_query_handler(text_contains="stop")
+async def buying_pear(call: CallbackQuery, reglet_id: str):
+    await call.answer(cache_time=60)
+    pass
+
+
+@dp.callback_query_handler(text_contains="reboot")
+async def buying_pear(call: CallbackQuery, reglet_id: str):
+    await call.answer(cache_time=60)
+    pass
+
+
+@dp.callback_query_handler(text_contains="delete")
+async def buying_pear(call: CallbackQuery, reglet_id: str):
+    await call.answer(cache_time=60)
+    pass
